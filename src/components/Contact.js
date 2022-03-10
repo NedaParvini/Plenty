cf41f700379fcd859c0b53cc5e90995ce0d471a0
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-03-09 20:04:40 -0800

    added navbar

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact">
+    <div class="container">
+      <h1 className="text-center mt-3">Contact</h1>
+    </div>
+  </div>
+  );
+}
+
+export default Contact;
