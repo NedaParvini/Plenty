3f497c530da6240e05ee1e18e5fafdc7b034d827
NedaParvini <nedaparvini1@gmail.com>
2022-03-15 11:19:06 -0700

    Add REACT Combo DropDown Menu

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as About } from "./About";
-export { default as Favorites } from "./Favorites";
+export { default as Form } from "./Form";
 export { default as Contact } from "./Contact";
 
