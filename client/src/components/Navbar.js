d38696d23389ea383a1a1e2acda2399a29c9dc83
NedaParvini <nedaparvini1@gmail.com>
2022-03-15 15:10:21 -0700

    Adding Some Styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 
+
 function Navbar() {
     return (
         <div className="navigation">
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <div className="container">
-            <NavLink className="navbar-brand" to="/">
-              Plenty
+            <NavLink className="navbar-brand" to="/" >
+            Plenty
             </NavLink>
             <div>
               <ul className="navbar-nav ml-auto">
@@ -36,9 +37,10 @@ function Navbar() {
               </ul>
             </div>
           </div>
-        </nav>
+           </nav>
       </div>
+      
     );
   }
-
+  
 export default Navbar;
