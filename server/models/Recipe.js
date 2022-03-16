0a51bbdcd0b7652b959e155dc1eb88b25bb307a6
Dee <dgrowette@gmail.com>
2022-03-15 21:18:07 -0700

    commit before a pull

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,1 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+let dropdown = document.getElementById("Recipe");
+dropdown.length = 0;
+let defaultOption = document.createElement("option");
+defaultOption.text = "Choose your ingredients";
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+const url = REACT_APP_API_KEY;
+fetch(url)
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.warn(
+        "Looks like there was a problem. Status Code: " + response.status
+      );
+      return;
+    }
+    // Examine the text in the response
+    response.json().then(function (data) {
+      let option;
+      for (let i = 0; i < data.length; i++) {
+        option = document.createElement("option");
+        option.text = data[i].name;
+        option.value = data[i].abbreviation;
+        dropdown.add(option);
+      }
+    });
+  })
+  .catch(function (err) {
+    console.error("Fetch Error -", err);
+  });
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,39 +1,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    recipeCount: Int
-    savedRecipes: [Recipe]
-  }
-
-  type Query {
-    me: User
-  }
-
-  type Recipe {
-    _id: ID
-    # // add more here- depends on API
-  }
-
-  type SavedRecipes {
-    recipeId: String
-    # // add more here- depends on API
-  }
-
-  type Auth {
-    token: ID!
-    user: User
-  }
-
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveRecipe(input: SavedRecipes!): User
-    removeRecipe(recipeId: String!): User
-  }
+type User {
+  _id: ID
+  username: String
+  email: String
+  recipeCount: Int
+  savedRecipes: [Recipe]
+}
+type Query {
+  idIngredient: id!
+}
+type Recipe {
+  _id: ID
+  # // add more here- depends on API
+}
+type SavedRecipes {
+  recipeId: String
+  # // add more here- depends on API
+}
+type Auth {
+  token: ID!
+  user: User
+}
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+  saveRecipe(input: SavedRecipes!): User
+  removeRecipe(recipeId: String!): User
+}
 `;
 
 module.exports = typeDefs;
