bc13cb48e7eb9e8bf74b763c700df3e1fdeba804
NedaParvini <nedaparvini1@gmail.com>
2022-03-16 15:31:48 -0700

    Added CSS and HTML codes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,41 @@ function Home() {
   return (
     <div className="home" id="homebackg">
       <div class="containerrr">
-        
+        <container class="contclassF"></container>
         <container class="contclass">
           <div>
             This application is built to help you find lots of recipes based on
-            the ingredients you choose. If you have a full refrigerator or
-            pantry but still don’t know what to cook for breakfast, lunch or
-            dinner, this app is the perfect Meal Assistant!
+            the ingredients you choose.
           </div>
-          </container>
-          
-          <container class="contclass2"><div></div>
         </container>
-        
-          <container class="contclass3"><div></div>
+
+        <container class="contclass2">
+          <div>
+            
+          </div>
         </container>
+
+        <container class="contclass3">
+          <div></div>
+        </container>
+
+        <container class="contclass4">
+          <div></div>
+        </container>
+
+        <container class="contclass5">
+          <div>MOST FAMOUS RECIPES</div>
+        </container>
+
+        <container class="contclass6">
+          <div>If you have a full refrigerator or pantry but still don’t know what
+            to cook for breakfast, lunch or dinner, this app is the perfect Meal
+            Assistant!</div>
+        </container>
+
       </div>
     </div>
   );
-};
-
-  
+}
 
 export default Home;
