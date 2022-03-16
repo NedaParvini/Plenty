9a86d294b31f34235fccd7a2aff1b8cde8075ac7
Dee <dgrowette@gmail.com>
2022-03-15 21:18:07 -0700

    commit before a pull

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+<<<<<<< HEAD
   type User {
     _id: ID
     username: String
@@ -35,6 +36,36 @@ const typeDefs = gql`
     # saveRecipe(input: SavedRecipes!): User
     # removeRecipe(recipeId: String!): User
   }
+=======
+type User {
+  _id: ID
+  username: String
+  email: String
+  recipeCount: Int
+  savedRecipes: [Recipe]
+}
+type Query {
+  idIngredient: id!
+}
+type Recipe {
+  _id: ID
+  # // add more here- depends on API
+}
+type SavedRecipes {
+  recipeId: String
+  # // add more here- depends on API
+}
+type Auth {
+  token: ID!
+  user: User
+}
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+  saveRecipe(input: SavedRecipes!): User
+  removeRecipe(recipeId: String!): User
+}
+>>>>>>> 0a51bbdc... commit before a pull
 `;
 
 module.exports = typeDefs;
