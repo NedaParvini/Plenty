e2b4a5248193e10c073a724e08a79de66bcd9bd8
NedaParvini <nedaparvini1@gmail.com>
2022-03-16 17:40:39 -0700

    CSS Classes have been modified

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import React from "react";
 function About() {
   return (
     <div className="about">
-    <div class="container">
+    <div className="container">
       <h1>About</h1>
-      <div class="aboutclass"><div id="aboutus_content"><p>This application is built to help you find lots of recipes based on the ingredients you choose.
+      <div className="aboutclass"><div id="aboutus_content"><p>This application is built to help you find lots of recipes based on the ingredients you choose.
 
 If you have a full refrigerator or pantry but still don’t know what to cook for breakfast, lunch or dinner, this app is the perfect Meal Assistant!
 </p></div>
