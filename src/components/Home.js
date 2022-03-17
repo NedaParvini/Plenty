1fa8bf69eb71eeafc1040a934d0b31f63433f45b
NedaParvini <nedaparvini1@gmail.com>
2022-03-16 18:17:25 -0700

    ClassName was the issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 function Home() {
   return (
     <div className="home" id="homebackg">
-      <div class="containerrr">
+      <div className="containerrr">
         <container className="contclassF"></container>
         <container className="contclass">
           <div>
