91696442f96d5bb8c10ecefe8b5309ffb6139c0b
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-03-19 18:55:50 -0700

    added utils folder to client/src which includes auth, mutations and queries

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 function Login() {
     const loginFormHandler = async function (event) {
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 function Signup() {
 
