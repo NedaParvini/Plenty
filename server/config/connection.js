ef3d83370467a89333a318ebaa3dcf22fa7620bb
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-03-20 19:21:37 -0700

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb://localhost/plenty' || "mongodb+srv://analuciarojas:Photography0919@cluster0.z4ftm.mongodb.net/plenty?retryWrites=true&w=majority",
+  process.env.MONGODB_URI || 'mongodb://localhost/plenty' || "mongodb+srv://analuciarojas:Photography0919@cluster0.z4ftm.mongodb.net/plenty?retryWrites=true&w=majority",
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
