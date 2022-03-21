8454c7f0f76997de7f0a07aa6546a42cf951fcca
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-03-20 22:09:32 -0700

    form for now

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,9 +3,8 @@ import React, { useEffect } from "react";
 import { MultiSelect } from "@progress/kendo-react-dropdowns";
 import { useState } from "react";
 import '@progress/kendo-theme-default/dist/all.css';
-import { Card, Container, Button, ListGroupItem, ListGroup, Row } from 'react-bootstrap';
+import { Card, Container, Button, ListGroupItem, ListGroup, Row,Col } from 'react-bootstrap';
 import { FaHandHoldingHeart } from 'react-icons/fa';
-import Col from "./Col";
 
 function Form() {
   //added here
@@ -28,6 +27,9 @@ function Form() {
   const [selectedAssignee, setSelectedAssignee] = useState(null);
   const onChange = event => setSelectedAssignee(event.value);
   const [meals, setMeals] = useState([])
+  const [style,setStyle] = useState({display: 'none'});
+  const [style1,setStyle1] = useState({display: 'block'});
+
   // on submit function/ take selected ingredients and send it to our other api, then render results.
 
   return (
@@ -39,29 +41,58 @@ function Form() {
           <label className="k-label k-mb-3">Choose your ingredients</label>
           <MultiSelect data={ingredients} value={selectedIngredients} onChange={onIngredientChange} autoClose={false}></MultiSelect>
           {console.log(selectedIngredients)}
-          <button type="submit" onClick={() => fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${selectedIngredients.join(',')}`)
+          <button type="submit" onClick={() => fetch(`https://www.themealdb.com/api/json/v2/1/filter.php?i=${selectedIngredients.join(',')}`)
+            .then(console.log(`https://www.themealdb.com/api/json/v2/1/filter.php?i=${selectedIngredients.join(',')}`))
             .then(response => response.json())
             .then(recipes => {
               console.log(recipes)
-              setMeals(recipes.meals)
-            })
-            .catch(error => console.log(error))}
-          >Submit</button>
-          <Container>
-            {meals !== undefined && meals.length > 0 ?
-              meals.map((meal, index) => { 
-              if(index % 2 === 0) {
-                return (<Row xs={1} md={2} className="g-4">
-                <Col meals={meal}>
-                
-                </Col>
-            </Row>
-            )
+              if(recipes!==null){
+                setMeals(recipes.meals)
               }
-              return (
-                <Col meals={meal}></Col>
-              )
-              }): <div></div> 
+            })
+          }
+          >Submit</button>
+          
+          <Container>
+            {meals !== null && meals.length > 0 ?
+                <Row xs={1} md={2} className="g-2">
+                    {(meals).map((meal) => (
+                    <Col>
+                    <Card sm='4' style={style1}>
+                      <Card.Img variant="top" src={`${meal.strMealThumb}`} />
+                      <Card.Title>{meal.strMeal}</Card.Title>
+                     <Card.Body>
+                      <Card.Text onClick={
+                          () => {
+                            { fetch(`https://www.themealdb.com/api/json/v2/1/lookup.php?i=${meal.idMeal}`)
+                              .then(response => {
+                                return response.json()
+                              })
+                              .then(recipes => {
+                                console.log(recipes)
+                                setMeals(recipes.meal)
+                              })
+                              console.log('here is meal id: ', meal.idMeal)
+                              setStyle({display: 'block'});
+                              setStyle1({display: 'none'});
+                          }
+                          }
+                        }></Card.Text>
+                    </Card.Body>
+                      <Button id={`${meal.idMeal}`} variant="primary">Favorite <FaHandHoldingHeart /></Button>
+                    </Card>
+
+                    <Card sm='4' style={style}>
+                      <Card.Img variant="top" src={`${meal.strMealThumb}`} />
+                        <Card.Title>{meal.strMeal}</Card.Title>
+                      <Card.Body>
+                        <Card.Text >YES</Card.Text>
+                      </Card.Body>
+                    </Card>
+                    </Col>
+                  )) }
+                </Row>
+                : selectedIngredients.length!==0 ? <div> There are no Available recepies with this ingredients...</div> : <div></div>
                 }
     
         </Container>
@@ -72,3 +103,10 @@ function Form() {
 }
 
 export default Form;
+
+
+// <ListGroup className="list-group-flush">
+//<ListGroupItem>Nationality: {meal.strArea} </ListGroupItem>
+//<ListGroupItem>Cuisine: {meal.strCategory}</ListGroupItem>
+//</ListGroup>
+
